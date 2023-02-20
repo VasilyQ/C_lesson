7ad2cff79f2fdde0c935c07da267c590c801eb8d
Vasily Petrunin <vasaaa7777@gmail.com>
2023-02-20 21:14:15 +0400

    add Example 012, FOR

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -1,54 +1,65 @@
-﻿ //1 Вид
- void Method1()
- {
-     Console.WriteLine("Автор...");
- }
- Method1();
- //Метод ничего не возращает
-
-
-// 2 Вид 
- void Method2(string msg)
- {
-     Console.WriteLine(msg);
- }
+﻿  //1 Вид
+  void Method1()
+  {
+      Console.WriteLine("Автор...");
+  }
+  Method1();
+  //Метод ничего не возращает
 
 
 
- void Method21(string msg, int count)
+  
+ // 2 Вид 
+  void Method2(string msg)
+  {
+      Console.WriteLine(msg);
+  }
+  void Method21(string msg, int count)
+  {
+      int i = 0;
+      while(i < count)
+      {
+          Console.WriteLine(msg);
+          i++;
+      }
+  }
+  Method21("Текс", 4);
+ // метод принимает агрументы
+ // Вид 3
+  int Method3()
+  {
+      return DateTime.Now.Year;
+  }
+  int year = Method3();
+  Console.WriteLine(year);
+
+
+
+ // 4 Вид
+ string Method4(int count, string c)
  {
      int i = 0;
+     string result = String.Empty; 
+     //String.Emply значит пустая строка все равно что ""
      while(i < count)
      {
-         Console.WriteLine(msg);
+         result = result + c;
          i++;
      }
+     return result;
  }
- Method21("Текс", 4);
-// метод принимает агрументы
+ string res = Method4(10, "z");
+ Console.WriteLine(res);
+ // Принимает аргументы и возращает
 
-// Вид 3
- int Method3()
- {
-     return DateTime.Now.Year;
- }
- int year = Method3();
- Console.WriteLine(year);
 
-// 4 Вид
 
-string Method4(int count, string c)
+// Цикл for, цикл в цикле
+for(int i = 2; i < 10; i++)
 {
-    int i = 0;
-    string result = String.Empty; 
-    //String.Emply значит пустая строка все равно что ""
-    while(i < count)
+    for(int j = 2; j <= 10; j++)
     {
-        result = result + c;
-        i++;
+        Console.WriteLine($"{i} * {j} = {i*j}");  
     }
-    return result;
+    Console.WriteLine();
 }
-string res = Method4(10, "z");
-Console.WriteLine(res);
-// Принимает аргументы и возращает
